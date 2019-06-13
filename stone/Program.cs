14dd414dca4ee5a6cc449a0329c34a08c9c148cb
leandromoh <leandromoh@hotmail.com>
2019-06-13 00:40:40 -0300

    initial commit. project created

---
+++ stone/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace stone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
