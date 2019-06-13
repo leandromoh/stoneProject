ffc0da0cb07e9a7f26d6cb3a1865e3e441ad1495
leandromoh <leandromoh@hotmail.com>
2019-06-13 01:50:45 -0300

    create IServices library

---
+++ IServices/ILoremUpsumService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IServices
+{
+    public enum ParagraphSize
+    {
+        Short = 1,
+        Medium = 2,
+        Long = 3,
+    }
+
+    public interface ILoremIpsumService
+    {
+        string GenerateLoremIpsum(bool startWithLoremIpsum, int quantityOfParagraphs, ParagraphSize paragraphSize);
+    }
+}
