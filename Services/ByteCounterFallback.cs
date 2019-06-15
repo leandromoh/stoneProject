4f3db0b54d51c54e13388d9020c9a629835936d1
leandromoh <leandromoh@hotmail.com>
2019-06-15 17:36:02 -0300

    added Services/ByteCounterFallback.cs

---
+++ Services/ByteCounterFallback.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using IServices;
+using System.Text;
+
+namespace Services
+{
+    public class ByteCounterFallback : IByteCounterService
+    {
+        private readonly UTF8Encoding encoding = new UTF8Encoding(); 
+
+        public long CountBytes(string text)
+        {
+            return encoding.GetBytes(text).LongLength;
+        }
+    }
+}
